be6693968bd5e4683b0753714aa58b877c111e47
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-27 05:56:11 +0200

    Testing Asynchronous Code

---
+++ src/components/Async.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+import Async from "./Async";
+
+describe("Async component", () => {
+  test("renders posts if request succeed", async () => {
+    // Arrange
+    render(<Async />);
+
+    // Act
+    // ... nothing
+
+    // Assert
+    const listItemElements = await screen.findAllByRole("listitem");
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
